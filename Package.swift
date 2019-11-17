82bcd146f3e869fb5819c4c34a68cc58bb3081ce
Joe Fabisevich <Joe@fabisevi.ch>
2019-11-16 21:08:11 -0500

    Updating Package.swift and podspec

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-  name: "Communicado"
+    name: "Communicado",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "Communicado",
+            targets: ["Communicado"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Communicado",
+            dependencies: [])
+    ]
 )
